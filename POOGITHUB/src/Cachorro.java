89570fde976fd686f2dda25cd94392bc74c405ac
luism <luism@LuisMiguel2010>
2026-06-09 11:45:16 -0300

    Projeto "Finalizado"

--- POOGITHUB/src/Cachorro.java
+++ POOGITHUB/src/Cachorro.java
@@ -1,94 +1,14 @@
+public class Cachorro extends Animal {
 
-	public class Cachorro {
-		 private String nome; 
-	     private String raca; 
-		 private Cliente dono; 
-		 private int idade; 
-		 private static int totalCachorro; 
-		 private boolean faminto = true;  
-		 public Cachorro(String nome,String raca, int idade) { 
-			 if (nome != "") { 
-					this.nome = nome;
-			 }else { 
-					System.out.println("Voce nao tem dono");
-					System.exit(0);
-			 }
-			 this.raca = raca; 
-			 if(idade>= 0) { 
-					this.idade = idade; 
-			 }else { 
-					System.out.println("Invalido idade negativa");	
-			 }
-			 totalCachorro++; 
-		 }
-		 
-		 public String getNome() {
-				return nome;
-		 }
-		public void setNome(String nome) {
-			if (nome != "") { 
-				this.nome = nome;
-			}else { 
-				System.out.println("Voce nao tem dono");
-			}
-		}
-			public String getRaca() {
-				return raca;
-			}
-			public void setRaca(String raca) {
-				this.raca = raca;
-			}
-			public Cliente getDono() {
-				return dono;
-			}
-			public void setDono(Cliente dono) {
-				this.dono = dono;
-			}
-			public int getIdade() {
-				return idade;
-			}
-			public void setIdade(int idade) {
-				if(idade>= 0) { 
-					this.idade = idade; 
-				}else { 
-					System.out.println("Invalido idade negativa");
-				}
-			} 
-			public static int getTotalCachorro() { 
-				return totalCachorro; 
-			}
-			public void setTotalCachorro(int totalCachorro) {
-				Cachorro.totalCachorro = totalCachorro;
-			}
-			public boolean isFaminto() {
-				return faminto;
-			}
-			public void setFaminto(boolean faminto) {
-				this.faminto = faminto;
-			}
-		 public void comer() { 
-			 if (faminto = true) { 
-				 System.out.printf("O " +nome+ " comeu!");
-				 faminto = false; 
-			 }else{ 
-				 System.out.printf(nome+ " ja esta satisfeito"); 
-			 }
-		 }
-		 public void latir() { 
-			 System.out.printf("O " +raca+ " chamado " +nome+ " diz: au au!");
-		 }
-		 public void exibirDados() { 
-			 System.out.printf("Nome:" + this.nome);
-			 System.out.println();
-           System.out.printf("Raca:" + this.raca);
-           System.out.println();
-           System.out.printf("Idade:" + this.idade);
-           System.out.println();
-           System.out.printf("Fome:" + this.faminto);
-		 }
+    private String raca;
 
-	}
-		
+    public Cachorro(String nome, String raca, int idade) {
+        super(nome, idade);
+        this.raca = raca;
+    }
 
-
-
+    @Override
+    public void emitirSom() {
+        System.out.println("AU AU!");
+    }
+}
--- POOGITHUB/src/ClinicaVeterenaria.java
+++ POOGITHUB/src/ClinicaVeterenaria.java
@@ -1,27 +1,15 @@
 
 public class ClinicaVeterenaria {
 
-		public static void main(String[] args) {
-			   Cachorro Cachorro1 = new Cachorro("", "Caramelo", 17); 
-			   Cachorro Cachorro2 = new Cachorro("Verstarppen", "Caramelo", 15); 
-			   Cliente Cliente1 = new Cliente("Mike", "Iphone"); 
-			   Cliente Cliente2 = new Cliente("Bolsonaro", "Samgsung"); 
-		       Cachorro1.setDono(Cliente1); 
-		       Cachorro2.setDono(Cliente2);
-		       System.out.printf(" O nome do tutor" +Cachorro1.getDono().getNome());
-		       System.out.println();
-		       Cachorro1.exibirDados();
-		       System.out.println(); 
-		       Cachorro2.exibirDados();
-		       System.out.println();
-		       System.out.printf("Total de Cachorros:" +Cachorro.getTotalCachorro());
-		      
-		      
-		 
-		      
-		       
+    public static void main(String[] args) {
 
-		}
+        Animal[] filaAtendimento = new Animal[2];
 
-	}
+        filaAtendimento[0] = new Cachorro("Rex", "Pastor Alemao", 5);
+        filaAtendimento[1] = new Gato("Mingau", 2, true);
 
+        for (Animal animal : filaAtendimento) {
+            animal.emitirSom();
+        }
+    }
+}
